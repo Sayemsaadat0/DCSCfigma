f1eb631d38007bb394b4f5deb87f23a210901451
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-01 22:42:17 +0600

    ew

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#4A6CDB',
-        secondary: '#02b6c7',
+        primary: "#4A6CDB",
+        secondary: "#02b6c7",
       },
     },
   },
   plugins: [require("daisyui")],
-}
+};
