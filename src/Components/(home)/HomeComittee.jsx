2d7a0cab01d4169d7c243d3dbf683e2b40180f8d
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-02 21:26:47 +0600

    new edits

--- src/Components/(home)/HomeComittee.jsx
+++ src/Components/(home)/HomeComittee.jsx
@@ -29,20 +29,18 @@ const HomeComittee = () => {
     return (
         <div className=' text-center max-w-[1500px] mx-auto relative'>
             <SectionTitle bigText={'Executive Committee 2023'} smallText={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when'}></SectionTitle>
-            {/* <div className='circle opacity-30 md:w-[100] md:h-[100] left-0 bottom-0 w-full h-full  absolute '></div> */}
+            <div className='circle opacity-30 md:w-[100] md:h-[100] left-0 bottom-0 w-full h-full  absolute '></div>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-5 px-4 mt-20 z-20 '>
-                {
-                    userData.map((data, index) => <div key={index} className="card rounded-lg z-10">
-                        <Image className='relative rounded-lg' src={data.image} alt='ss' width={400} height={400}></Image>
-                        <div className='text-left cards ps-6 '>
-
-                        </div>
-                        <div className='text-left z-20 px-4 pb-4'>
+               {userData.map((data, index) => (
+                    <div key={index} className="card hover:scale-105 duration-700 rounded-lg group z-10">
+                        <Image className='relative rounded-lg' src={data.image} alt='ss' width={400} height={400} />
+                        <div className='text-left cards ps-6'></div>
+                        <div className='text-left z-20 px-4 pb-4 group-hover:-translate-y-10 duration-700'>
                             <p className='text-xl font-bold'>{data.name}</p>
                             <p className='text-secondary'>{data.designation}</p>
                         </div>
-                    </div>)
-                }
+                    </div>
+                ))}
             </div>
 
             <div className='my-20'>
